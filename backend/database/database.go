0ae482817c9de2eac8f24768ca8d572fd9e51eb6
M Mubashir <muhammadmu062@gmail.com>
2026-08-12 14:42:33 +0200

    adding new attribute in database

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,9 +1,40 @@
+// package database
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectDataBase() {
+// 	dsn := os.Getenv("DATABASE_URL")
+// 	database, err := gorm.Open(
+// 		postgres.Open(dsn),
+// 		&gorm.Config{},
+// 	)
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Failed to Connect to database")
+// 	} else {
+// 		DB = database
+// 		fmt.Println("Successfully Connected To Database")
+// 	}
+
+// }
 package database
 
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,16 +42,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDataBase() {
-	dsn := "postgres://postgres:TUMHARA_PASSWORD@localhost:5432/expense_tracker"
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found sorry")
+	}
+	dsn := os.Getenv("DATABASE_URL")
 	database, err := gorm.Open(
 		postgres.Open(dsn),
 		&gorm.Config{},
 	)
 	if err != nil {
 		log.Fatal("Failed to Connect to database")
-	} else {
-		DB = database
-		fmt.Println("Successfully Connected To Database")
 	}
+	fmt.Println("Database Successfully Connected")
+	DB = database
 
 }
